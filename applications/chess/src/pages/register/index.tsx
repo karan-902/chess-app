48b385350e17f3c06131b3abf8927cdbbe08cb6b
karan-902 <dhakadmac@gmail.com>
2026-09-10 17:49:11 +0530

    fix one

--- applications/chess/src/pages/register/index.tsx
+++ applications/chess/src/pages/register/index.tsx
@@ -16,7 +16,7 @@ export default function Register() {
    footer={
     <>
      {authRegisterHaveAccountPrompt}
-     {"Sai"}
+
      <Link to="/login">{authRegisterLoginLink}</Link>
     </>
    }
